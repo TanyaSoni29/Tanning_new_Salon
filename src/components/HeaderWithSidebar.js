d4f7536eab0f8a2c0e53daa2c5db76a073370634
abhivlogsingh <abhisheksingh4115@gmail.com>
2024-09-30 13:12:28 +0530

    updated nav formate

--- src/components/HeaderWithSidebar.js
+++ src/components/HeaderWithSidebar.js
@@ -6,9 +6,24 @@ import { NavLink } from 'react-router-dom';
 
 const HeaderWithSidebar = () => {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [isReportSubmenuOpen, setIsReportSubmenuOpen] = useState(false);
+	const [isCustomersSubmenuOpen, setIsCustomersSubmenuOpen] = useState(false);
+	const [isTransactionsSubmenuOpen, setIsTransactionsSubmenuOpen] = useState(false);
 
 	const toggleSidebar = () => {
 		setIsSidebarOpen(!isSidebarOpen);
+	};
+
+	const toggleReportSubmenu = () => {
+		setIsReportSubmenuOpen(!isReportSubmenuOpen);
+	};
+
+	const toggleCustomersSubmenu = () => {
+		setIsCustomersSubmenuOpen(!isCustomersSubmenuOpen);
+	};
+
+	const toggleTransactionsSubmenu = () => {
+		setIsTransactionsSubmenuOpen(!isTransactionsSubmenuOpen);
 	};
 
 	return (
@@ -43,25 +58,143 @@ const HeaderWithSidebar = () => {
 				</div>
 				<ul className='sidebar-menu'>
 					<li>
-						<NavLink to='/locationStep'>Dashboard</NavLink>
+						<NavLink
+							to='/locationStep'
+							className={({ isActive }) => (isActive ? 'active' : '')}
+						>
+							Dashboard
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/location'>Location</NavLink>
+						<NavLink
+							to='/location'
+							className={({ isActive }) => (isActive ? 'active' : '')}
+						>
+							Location
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/users'>Users</NavLink>
+						<NavLink
+							to='/users'
+							className={({ isActive }) => (isActive ? 'active' : '')}
+						>
+							Users
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/products'>Products</NavLink>
+						<NavLink
+							to='/products'
+							className={({ isActive }) => (isActive ? 'active' : '')}
+						>
+							Products
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/services'>Services</NavLink>
+						<NavLink
+							to='/services'
+							className={({ isActive }) => (isActive ? 'active' : '')}
+						>
+							Services
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/customers'>Customers</NavLink>
+						<NavLink
+							to='/customers'
+							className={({ isActive }) => (isActive ? 'active' : '')}
+						>
+							Customers
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/transactions'>Transactions</NavLink>
+						{/* Report with Submenu */}
+						<div className='submenu-item'>
+							<span
+								className='submenu-title'
+								onClick={toggleReportSubmenu}
+							>
+								Report <i className={`fa ${isReportSubmenuOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+							</span>
+							{isReportSubmenuOpen && (
+								<ul className='submenu'>
+									{/* Customers Submenu */}
+									<li className='submenu-item'>
+										<span
+											className='submenu-title'
+											onClick={toggleCustomersSubmenu}
+										>
+											Customers{' '}
+											<i className={`fa ${isCustomersSubmenuOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+										</span>
+										{isCustomersSubmenuOpen && (
+											<ul className='submenu'>
+												<li>
+													<NavLink
+														to='/reports/customers/all'
+														className={({ isActive }) => (isActive ? 'active' : '')}
+													>
+														All Customers
+													</NavLink>
+												</li>
+												<li>
+													<NavLink
+														to='/reports/customers/by-date'
+														className={({ isActive }) => (isActive ? 'active' : '')}
+													>
+														By Date
+													</NavLink>
+												</li>
+												<li>
+													<NavLink
+														to='/reports/customers/current-month'
+														className={({ isActive }) => (isActive ? 'active' : '')}
+													>
+														Current Month
+													</NavLink>
+												</li>
+												<li>
+													<NavLink
+														to='/reports/customers/top-customers'
+														className={({ isActive }) => (isActive ? 'active' : '')}
+													>
+														Top Customers
+													</NavLink>
+												</li>
+											</ul>
+										)}
+									</li>
+									{/* Transactions Submenu */}
+									<li className='submenu-item'>
+										<span
+											className='submenu-title'
+											onClick={toggleTransactionsSubmenu}
+										>
+											Transactions{' '}
+											<i className={`fa ${isTransactionsSubmenuOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+										</span>
+										{isTransactionsSubmenuOpen && (
+											<ul className='submenu'>
+												<li>
+													<NavLink
+														to='/reports/transactions/services-used'
+														className={({ isActive }) => (isActive ? 'active' : '')}
+													>
+														Services Used
+													</NavLink>
+												</li>
+												<li>
+													<NavLink
+														to='/reports/transactions/sales'
+														className={({ isActive }) => (isActive ? 'active' : '')}
+													>
+														Sales
+													</NavLink>
+												</li>
+											</ul>
+										)}
+									</li>
+								</ul>
+							)}
+						</div>
 					</li>
 				</ul>
 			</div>
