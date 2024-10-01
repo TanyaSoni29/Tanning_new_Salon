9fd165214266d2209579b538fe0dc88258b098eb
TanyaSoni2912 <tanu139sona@gmail.com>
2024-10-01 16:07:04 +0530

    Qr Locations are fetch from Api

--- src/components/Qrcode/QrcodeList.js
+++ src/components/Qrcode/QrcodeList.js
@@ -1,76 +1,79 @@
-import React, { useState } from 'react';
+/** @format */
+
+import React, { useEffect, useState } from 'react';
 import { QRCodeCanvas } from 'qrcode.react';
 import './QrcodeList.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { refreshLocation } from '../../slices/locationSlice';
 
 const QrcodeList = () => {
-  const [locations] = useState([
-    {
-      locationName: 'Location 1',
-    },
-    {
-      locationName: 'Location 2',
-    },
-    {
-      locationName: 'Location 3',
-    },
-  ]);
+	const dispatch = useDispatch(); // Get the Redux dispatch function
 
-  const [selectedLocation, setSelectedLocation] = useState(null);
+	const { locations } = useSelector((state) => state.location);
 
-  const handleLocationClick = (index) => {
-    setSelectedLocation(locations[index]);
-  };
+	const [selectedLocation, setSelectedLocation] = useState(null);
 
-  const handlePrint = () => {
-    const printContent = document.getElementById('print-area');
-    const win = window.open('', '', 'height=500,width=800');
-    win.document.write('<html><head><title>Print QR Code</title>');
-    win.document.write('</head><body >');
-    win.document.write(printContent.innerHTML);
-    win.document.write('</body></html>');
-    win.document.close();
-    win.print();
-  };
+	useEffect(() => {
+		dispatch(refreshLocation());
+	}, [dispatch]);
 
-  return (
-    <div className="qrcode-container">
-      <h2>Select Your Location</h2>
-      <div className="location-circles">
-        {locations.map((location, index) => (
-          <div
-            key={index}
-            className="location-circle"
-            onClick={() => handleLocationClick(index)}
-          >
-            <div className="icon">
-              <i className="fa fa-laptop"></i>
-            </div>
-            <p>{location.locationName}</p>
-          </div>
-        ))}
-      </div>
+	const handleLocationClick = (index) => {
+		setSelectedLocation(locations[index]);
+	};
 
-      {selectedLocation && (
-        <div className="qr-code-display" id="print-area">
-          <h3>{selectedLocation.locationName}</h3>
-          {/* Dynamically generate a QR code with URL */}
-          <QRCodeCanvas
-            value={`https://backendcodersindia.com/${selectedLocation.locationName}`} // Encode a valid URL
-            size={150} // QR code size
-            className="qr-code-image"
-          />
-        </div>
-      )}
+	const handlePrint = () => {
+		const printContent = document.getElementById('print-area');
+		const win = window.open('', '', 'height=500,width=800');
+		win.document.write('<html><head><title>Print QR Code</title>');
+		win.document.write('</head><body >');
+		win.document.write(printContent.innerHTML);
+		win.document.write('</body></html>');
+		win.document.close();
+		win.print();
+	};
 
-      <button
-        className="next-button"
-        onClick={handlePrint}
-        disabled={!selectedLocation}
-      >
-        Print QR
-      </button>
-    </div>
-  );
+	return (
+		<div className='qrcode-container'>
+			<h2>Select Your Location</h2>
+			<div className='location-circles'>
+				{locations.map((location, index) => (
+					<div
+						key={index}
+						className='location-circle'
+						onClick={() => handleLocationClick(index)}
+					>
+						<div className='icon'>
+							<i className='fa fa-laptop'></i>
+						</div>
+						<p>{location.name}</p>
+					</div>
+				))}
+			</div>
+
+			{selectedLocation && (
+				<div
+					className='qr-code-display'
+					id='print-area'
+				>
+					<h3>{selectedLocation.locationName}</h3>
+					{/* Dynamically generate a QR code with URL */}
+					<QRCodeCanvas
+						value={`https://backendcodersindia.com/${selectedLocation.locationName}`} // Encode a valid URL
+						size={150} // QR code size
+						className='qr-code-image'
+					/>
+				</div>
+			)}
+
+			<button
+				className='next-button'
+				onClick={handlePrint}
+				disabled={!selectedLocation}
+			>
+				Print QR
+			</button>
+		</div>
+	);
 };
 
 export default QrcodeList;
