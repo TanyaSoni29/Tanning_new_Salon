0569dfc2df632b578d118bb5edd9ad26bc0fd511
TanyaSoni2912 <tanu139sona@gmail.com>
2024-10-02 17:33:32 +0530

    Service purchase is

--- src/components/Purchasereport/Purchasereport.js
+++ src/components/Purchasereport/Purchasereport.js
@@ -1,14 +1,44 @@
 /** @format */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderWithSidebar from '../HeaderWithSidebar';
 import ProductList from './PurchasereportList';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllServiceTransactions } from '../../service/operations/serviceAndServiceTransaction';
+import { refreshLocation } from '../../slices/locationSlice';
 
 function Purchasereport() {
+	const dispatch = useDispatch();
+	const { token } = useSelector((state) => state.auth);
+	const [purchaseServiceTransaction, setPurchaseServiceTransaction] = useState(
+		[]
+	);
+	useEffect(() => {
+		async function getPurchaseServiceTrnsaction() {
+			try {
+				const response = await getAllServiceTransactions(token);
+				setPurchaseServiceTransaction(
+					response
+						.filter(
+							(transaction) => transaction.transaction.type === 'purchased'
+						)
+						.sort(
+							(a, b) =>
+								new Date(b.transaction.created_at) -
+								new Date(a.transaction.created_at)
+						)
+				);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		getPurchaseServiceTrnsaction();
+		dispatch(refreshLocation());
+	}, []);
 	return (
 		<div>
 			<HeaderWithSidebar />
-			<ProductList />
+			<ProductList purchaseServiceTransaction={purchaseServiceTransaction} />
 		</div>
 	);
 }
--- src/components/Purchasereport/PurchasereportList.js
+++ src/components/Purchasereport/PurchasereportList.js
@@ -5,20 +5,57 @@ import './PurchasereportList.css'; // Importing CSS
 import { saveAs } from 'file-saver'; // For saving files
 import jsPDF from 'jspdf'; // For generating PDFs
 import { FaFileCsv, FaFilePdf } from 'react-icons/fa'; // Icons for CSV and PDF
+import { useSelector } from 'react-redux';
+import { formatDate } from '../../utils/formateDate';
 
-const ProductList = () => {
-	const [products, setProducts] = useState([
-		{
-			productName: 'Product 1',
-			price: '$100',
-			listedOn: '2024-09-30',
-		},
-	]);
+const ProductList = ({ purchaseServiceTransaction }) => {
+	const [dateRange, setDateRange] = useState({
+		startDate: null,
+		endDate: null,
+	});
+	const [selectedLocation, setSelectedLocation] = useState('All');
+	const { locations } = useSelector((state) => state.location);
 
 	const [searchTerm, setSearchTerm] = useState('');
 
-	const filteredProducts = products.filter((product) =>
-		product.productName.toLowerCase().includes(searchTerm.toLowerCase())
+	const uniqueLocations = [
+		'All',
+		...new Set(locations.map((location) => location.name)),
+	];
+
+	const handleDateRangeChange = (e) => {
+		const { name, value } = e.target;
+		setDateRange((prevRange) => ({
+			...prevRange,
+			[name]: value ? new Date(value) : null,
+		}));
+	};
+
+	const handleLocationChange = (e) => {
+		setSelectedLocation(e.target.value);
+	};
+
+	const filteredTransaction = purchaseServiceTransaction.filter(
+		(transaction) => {
+			const transactionDate = new Date(transaction.transaction.created_at);
+			const isInDateRange =
+				dateRange.startDate && dateRange.endDate
+					? transactionDate >= dateRange.startDate &&
+					  transactionDate <= dateRange.endDate
+					: true;
+			const firstName =
+				transaction?.user_details?.firstName?.toLowerCase() || '';
+			const lastName = transaction?.user_details?.lastName?.toLowerCase() || '';
+			const serviceName = transaction?.service?.name?.toLowerCase() || '';
+			const matchesSearchQuery =
+				`${firstName} ${lastName}`.includes(searchTerm.toLowerCase()) ||
+				serviceName.includes(searchTerm.toLowerCase());
+			const matchesLocation =
+				selectedLocation === 'All' ||
+				transaction.user_details?.preferred_location?.name === selectedLocation;
+
+			return isInDateRange && matchesSearchQuery && matchesLocation;
+		}
 	);
 
 	// const handleDelete = (index) => {
@@ -29,23 +66,37 @@ const ProductList = () => {
 
 	// Function to download CSV
 	const handleDownloadCSV = () => {
-		const headers = ['Product Name', 'Price', 'Listed On'];
+		const headers = [
+			'User Name',
+			'Service Name',
+			'Price',
+			'Quantity',
+			'Location',
+			'Date/Time',
+		];
 		const csvRows = [
 			headers.join(','), // header row
-			...products.map((product) =>
-				[product.productName, product.price, product.listedOn].join(',')
+			...filteredTransaction.map((data) =>
+				[
+					`${data.user_details.firstName} ${data.user_details.lastName}`,
+					data.service.name,
+					data.service.price,
+					data.transaction.quantity,
+					data.user_details.preferred_location?.name,
+					data.transaction.created_at,
+				].join(',')
 			),
 		].join('\n');
 
 		const blob = new Blob([csvRows], { type: 'text/csv;charset=utf-8;' });
-		saveAs(blob, 'products.csv');
+		saveAs(blob, 'service-purchase.csv');
 	};
 
 	// Function to download PDF
 	const handleDownloadPDF = () => {
 		const doc = new jsPDF();
 
-		doc.text('Product List', 10, 10); // Title of the document
+		doc.text('Service Used', 10, 10); // Title of the document
 		let row = 20;
 
 		// Table headers
@@ -55,10 +106,25 @@ const ProductList = () => {
 		row += 10;
 
 		// Table content
-		products.forEach((product) => {
-			doc.text(product.productName, 10, row);
-			doc.text(product.price, 80, row);
-			doc.text(product.listedOn, 140, row);
+		filteredTransaction.forEach((transaction) => {
+			doc.text(
+				`${transaction.user_details.firstName} ${transaction.user_details.lastName}`,
+				10,
+				row
+			);
+			doc.text(transaction.service?.name, 10, row);
+			doc.text(
+				`${transaction.user_details.firstName} ${transaction.user_details.lastName}`,
+				10,
+				row
+			);
+			doc.text(
+				`${transaction.user_details.firstName} ${transaction.user_details.lastName}`,
+				10,
+				row
+			);
+			doc.text(transaction.service.price, 80, row);
+			doc.text(transaction.transaction.created_at, 140, row);
 			row += 10;
 		});
 
@@ -74,34 +140,87 @@ const ProductList = () => {
 					value={searchTerm}
 					onChange={(e) => setSearchTerm(e.target.value)}
 				/>
+				<div className='date-range-inputs'>
+					<input
+						type='date'
+						name='startDate'
+						placeholder='Start Date'
+						onChange={handleDateRangeChange}
+					/>
+					<input
+						type='date'
+						name='endDate'
+						placeholder='End Date'
+						onChange={handleDateRangeChange}
+					/>
+				</div>
+				<div className='location-select'>
+					<select
+						value={selectedLocation}
+						onChange={handleLocationChange}
+					>
+						{uniqueLocations.map((location) => (
+							<option
+								key={location}
+								value={location}
+							>
+								{location}
+							</option>
+						))}
+					</select>
+				</div>
 				<div className='purchasereportlist-files'>
-
-					<div className='purchasereportlist-download' onClick={handleDownloadCSV}>
-					<FaFileCsv size={45} style={{ color: '#28a745' }} /> {/* Green for CSV */}
-					
-				</div>
-				<div className='purchasereportlist-download' onClick={handleDownloadPDF}>
-					<FaFilePdf size={45} style={{ color: '#dc3545' }} /> {/* Red for PDF */}	
-				</div>
+					<div
+						className='purchasereportlist-download'
+						onClick={handleDownloadCSV}
+					>
+						<FaFileCsv
+							size={45}
+							style={{ color: '#28a745' }}
+						/>{' '}
+						{/* Green for CSV */}
+					</div>
+					<div
+						className='purchasereportlist-download'
+						onClick={handleDownloadPDF}
+					>
+						<FaFilePdf
+							size={45}
+							style={{ color: '#dc3545' }}
+						/>{' '}
+						{/* Red for PDF */}
+					</div>
 				</div>
 			</div>
 
 			<div className='purchasereportlist-table'>
 				<div className='purchasereportlist-table-header'>
-					<span>PRODUCT NAME</span>
+					<span>USER NAME</span>
+					<span>SERVICE NAME</span>
 					<span>PRICE</span>
-					<span>LISTED ON</span>
+					<span>QUANTITY</span>
+					<span>LOCATION</span>
+					<span>DATE/TIME</span>
 				</div>
 
-				{filteredProducts.length > 0 ? (
-					filteredProducts.map((product, index) => (
+				{filteredTransaction.length > 0 ? (
+					filteredTransaction.map((transaction) => (
 						<div
-							key={index}
-							className='purchasereportlist-table-row'
+							key={transaction.transaction.id}
+							className='serviceused-table-row'
 						>
-							<span>{product.productName}</span>
-							<span>{product.price}</span>
-							<span>{product.listedOn}</span>
+							<span>
+								{transaction.user_details?.firstName}{' '}
+								{transaction.user_details?.lastName}
+							</span>
+							<span>{transaction.service.name}</span>
+							<span>{transaction.service.price}</span>
+							<span>{transaction.transaction.quantity}</span>
+							<span>{transaction.user_details.preferred_location?.name}</span>
+							<span style={{ whiteSpace: 'nowrap' }}>
+								{formatDate(transaction.transaction.created_at)}{' '}
+								{transaction.transaction.created_at.split('T')[1].slice(0, 8)}
+							</span>
 						</div>
 					))
 				) : (
