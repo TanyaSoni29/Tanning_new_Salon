9e2ddfffa31a375da3f6fe6724791a511e3c6ebc
Abhishek Singh <abhisheksingh4115@gmail.com>
2024-10-04 11:38:37 +0100

    updated css

--- src/components/Customers/ViewCustomerModal.js
+++ src/components/Customers/ViewCustomerModal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ViewCustomerModal.css"; // Importing CSS
-import { Card, IconButton, Typography, Box } from "@mui/material";
+import { Card, IconButton, Typography, Box, Button } from "@mui/material";
 import { useSelector } from "react-redux";
 import CloseIcon from "@mui/icons-material/Close";
 
@@ -20,47 +20,86 @@ const ViewCustomerModal = ({ closeViewModal, activeUser }) => {
       </IconButton>
 
       <Box className="viewcustomer-user-details">
-        {/* Name and Minutes Balance */}
-        <div className="viewcustomer-user-info">
+        <div className="viewcustomer-left-right">
+          {/* Name */}
           <Typography variant="h6" className="viewcustomer-info-label">
-            Name:{" "}
-            <Typography variant="h6" component="span" className="viewcustomer-info-value">
-              {activeUser.profile?.firstName + " " + activeUser.profile?.lastName}
-            </Typography>
+            Name:
           </Typography>
-
-          <Typography variant="h6" className="viewcustomer-info-label">
-            Minutes Balance:{" "}
-            <Typography variant="h6" component="span" className="viewcustomer-info-value">
-              {activeUser.profile?.available_balance || 0}
-            </Typography>
+          <Typography variant="h6" className="viewcustomer-info-value">
+            {activeUser.profile?.firstName + " " + activeUser.profile?.lastName}
           </Typography>
         </div>
 
-        {/* Phone Number and Total Spent */}
-        <div className="viewcustomer-user-info">
+        <div className="viewcustomer-left-right">
+          {/* Email */}
           <Typography variant="body2" className="viewcustomer-info-label">
-            Phone Number:{" "}
-            <Typography variant="body2" component="span" className="viewcustomer-info-value">
-              {activeUser.profile?.phone_number}
-            </Typography>
+            Email:
           </Typography>
-
-          <Typography variant="body2" className="viewcustomer-info-label">
-            Total Spent:{" "}
-            <Typography variant="body2" component="span" className="viewcustomer-info-value">
-              £{activeUser.profile?.total_spend?.toFixed(2) || 0}
-            </Typography>
+          <Typography variant="body2" className="viewcustomer-info-value">
+            {activeUser.user?.email}
           </Typography>
         </div>
 
-        {/* Gender */}
-        <Typography variant="body2" className="viewcustomer-info-label">
-          Gender:{" "}
-          <Typography variant="body2" component="span" className="viewcustomer-info-value">
+        <div className="viewcustomer-left-right">
+          {/* Phone Number */}
+          <Typography variant="body2" className="viewcustomer-info-label">
+            Phone Number:
+          </Typography>
+          <Typography variant="body2" className="viewcustomer-info-value">
+            {activeUser.profile?.phone_number}
+          </Typography>
+        </div>
+
+        <div className="viewcustomer-left-right">
+          {/* Role */}
+          <Typography variant="body2" className="viewcustomer-info-label">
+            Role:
+          </Typography>
+          <Typography variant="body2" className="viewcustomer-info-value">
+            {activeUser.user?.role}
+          </Typography>
+        </div>
+
+        <div className="viewcustomer-left-right">
+          {/* Address */}
+          <Typography variant="body2" className="viewcustomer-info-label">
+            Address:
+          </Typography>
+          <Typography variant="body2" className="viewcustomer-info-value">
+            {activeUser.profile?.address}
+          </Typography>
+        </div>
+
+        <div className="viewcustomer-left-right">
+          {/* Post Code */}
+          <Typography variant="body2" className="viewcustomer-info-label">
+            Post Code:
+          </Typography>
+          <Typography variant="body2" className="viewcustomer-info-value">
+            {activeUser.profile?.post_code}
+          </Typography>
+        </div>
+
+        <div className="viewcustomer-left-right">
+          {/* Gender */}
+          <Typography variant="body2" className="viewcustomer-info-label">
+            Gender:
+          </Typography>
+          <Typography variant="body2" className="viewcustomer-info-value">
             {activeUser.profile?.gender}
           </Typography>
-        </Typography>
+        </div>
+
+        {/* Buttons for Minutes Balance and Total Spent */}
+        <div className="viewcustomer-buttons">
+          <Button variant="contained" className="viewcustomer-button">
+            Minutes Balance: {activeUser.profile?.available_balance || 0}
+          </Button>
+
+          <Button variant="contained" className="viewcustomer-button">
+            Total Spent: £{activeUser.profile?.total_spend?.toFixed(2) || 0}
+          </Button>
+        </div>
       </Box>
     </Card>
   );
