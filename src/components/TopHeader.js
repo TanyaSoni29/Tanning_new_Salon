ef990fd0cfe5ffc38ad0b06c0ef765d82bb39bb1
Abhishek Singh <abhisheksingh4115@gmail.com>
2024-10-04 14:31:59 +0100

    updated  location section

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -1,8 +1,7 @@
-/** @format */
-
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
+import { useLocation } from 'react-router-dom'; // Import useLocation from react-router-dom
 import './TopHeader.css';
 
 function TopHeader({
@@ -19,20 +18,67 @@ function TopHeader({
 
 	// Extract the preferred location ID from the logged-in user profile
 	const preferredLocationId = userDetails?.profile?.preferred_location;
-	const locationName = locations.find((l) => l.id === preferredLocationId);
+
+	// Get the current route path using useLocation
+	const locationPath = useLocation().pathname;
+
+	// Filter locations based on the route (logic can be customized)
+	const filteredLocations = locations.filter((location) => {
+		// Example logic: Filter based on route name
+		if (locationPath === '/location') {
+			// Show only specific locations for '/route1'
+			return location.route === '/about';
+		} else if (locationPath === '/users') {
+			// Show only specific locations for '/route2'
+			return location.route === '/about';
+		} else if (locationPath === '/customers') {
+			// Show only specific locations for '/route2'
+			return location.route === '/about';
+		} else if (locationPath === '/products') {
+			// Show only specific locations for '/route2'
+			return location.route === '/about';
+		} else if (locationPath === '/services') {
+			// Show only specific locations for '/route2'
+			return location.route === '/about';
+		} else if (locationPath === '/allcustomers') {
+			// Show only specific locations for '/route2'
+			return location.route === '/about';
+		} else if (locationPath === '/currentmonth') {
+			// Show only specific locations for '/route2'
+			return location.route === '/about';
+		} else if (locationPath === '/productreport') {
+			// Show only specific locations for '/route2'
+			return location.route === '/about';
+		} else if (locationPath === '/purchasereport') {
+			// Show only specific locations for '/route2'
+			return location.route === '/about';
+		}  else if (locationPath === '/purchasereport') {
+			// Show only specific locations for '/route2'
+			return location.route === '/about';
+		} else {
+			// Default: Show all locations
+			return true;
+		}
+	});
+
+	// Find the location name based on the preferred location ID
+	const locationName = filteredLocations.find((l) => l.id === preferredLocationId);
+
 	useEffect(() => {
 		if (preferredLocationId) {
 			setSelectedLocation(preferredLocationId); // Set the default selected location
 		}
 	}, [preferredLocationId, setSelectedLocation]);
+
 	return (
 		<header className='top-header'>
+			
 			<select
 				value={selectedLocation}
 				onChange={handleLocationChange}
 				className='location-select'
 			>
-				{locations.map((location) => (
+				{filteredLocations.map((location) => (
 					<option
 						key={location.id}
 						value={location.id}
@@ -41,6 +87,7 @@ function TopHeader({
 					</option>
 				))}
 			</select>
+			
 			<div className='login-details'>
 				<FaUserCircle
 					size={28}
@@ -57,3 +104,6 @@ function TopHeader({
 }
 
 export default TopHeader;
+
+
+
