d59b673d6a5300c9ce294d6ace67565f21701677
TanyaSoni2912 <tanu139sona@gmail.com>
2024-10-05 13:25:59 +0530

    Last used Date

--- src/components/CustomerOverview.js
+++ src/components/CustomerOverview.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './CustomerOverview.css'; // Importing the separate CSS file
 import { useDispatch, useSelector } from 'react-redux';
 import { formatDate } from '../utils/formateDate';
@@ -9,9 +9,15 @@ import { useNavigate } from 'react-router';
 import { setCustomer, setCustomerIndex } from '../slices/customerProfile';
 import Modal from '../components/Modal';
 import QuestionCustomer from './QuestionCustomer';
+import {
+	getAllServiceTransactions,
+	getServiceTransactionsByUser,
+} from '../service/operations/serviceAndServiceTransaction';
 function CustomerOverview({ filteredCustomers }) {
 	const [isQuesModal, setIsQuesModal] = useState(false);
 	const dispatch = useDispatch();
+	const { token } = useSelector((state) => state.auth);
+	const [useServiceTransaction, setUseServiceTransaction] = useState([]);
 	const handleSelectButton = (user) => {
 		dispatch(setCustomer(user));
 		setIsQuesModal(true);
@@ -19,6 +25,32 @@ function CustomerOverview({ filteredCustomers }) {
 
 	const handleCloseQuesModal = () => {
 		setIsQuesModal(false);
+	};
+
+	useEffect(() => {
+		async function getServiceTransaction() {
+			const response = await getAllServiceTransactions(token);
+			setUseServiceTransaction(
+				response.filter((data) => data.transaction.type === 'used')
+			);
+		}
+		getServiceTransaction();
+	}, [filteredCustomers]);
+
+	const getLastUsedServiceDate = (customerId) => {
+		const userTransactions = useServiceTransaction.filter(
+			(transaction) => transaction.user_details.id === customerId
+		);
+		if (userTransactions.length > 0) {
+			// Sort transactions by date and return the most recent one
+			const lastTransaction = userTransactions.sort(
+				(a, b) =>
+					new Date(b.transaction.created_at) -
+					new Date(a.transaction.created_at)
+			)[0];
+			return lastTransaction.transaction.created_at;
+		}
+		return null; // If no transactions found
 	};
 
 	// Filter customers based on the search query
@@ -64,34 +96,33 @@ function CustomerOverview({ filteredCustomers }) {
 						</thead>
 						<tbody>
 							{filteredCustomers.length > 0 ? (
-								filteredCustomers.map((data) => (
-									<tr key={data.user?.id}>
-										<td>{data.profile?.firstName}</td>
-										<td>
-											{data.profile?.phone_number
-												? data.profile?.phone_number
-												: '-'}
-										</td>
-										<td>
-											{data.profile?.available_balance}
-										</td>
-										<td>{data?.total_used_minutes}</td>
-										<td className='tdcustomer'>£{data?.total_service_purchased_price?.toFixed(2)}</td>
-										<td>
-											{data?.profile?.updated_at
-												? formatDate(data?.profile?.updated_at)
-												: 'N/A'}
-										</td>
-										<td>
-											<Button
-												className='select-button'
-												onClick={() => handleSelectButton(data)}
-											>
-												Select
-											</Button>
-										</td>
-									</tr>
-								))
+								filteredCustomers.map((data) => {
+									const lastUsedDate = getLastUsedServiceDate(data.user?.id);
+									return (
+										<tr key={data.user?.id}>
+											<td>{data.profile?.firstName}</td>
+											<td>
+												{data.profile?.phone_number
+													? data.profile?.phone_number
+													: '-'}
+											</td>
+											<td>{data.profile?.available_balance}</td>
+											<td>{data?.total_used_minutes}</td>
+											<td className='tdcustomer'>
+												£{data?.total_service_purchased_price?.toFixed(2)}
+											</td>
+											<td>{lastUsedDate ? formatDate(lastUsedDate) : 'N/A'}</td>
+											<td>
+												<Button
+													className='select-button'
+													onClick={() => handleSelectButton(data)}
+												>
+													Select
+												</Button>
+											</td>
+										</tr>
+									);
+								})
 							) : (
 								<tr>
 									<td
