bac9e1d6eeccd69e0bab774a1d17e4404dae26f4
TanyaSoni29 <tanu139sona@gmail.com>
2024-10-08 21:59:35 +0530

    Current Month

--- src/components/Currentmonth/Currentmonth.js
+++ src/components/Currentmonth/Currentmonth.js
@@ -1,26 +1,26 @@
 /** @format */
 
-import React, { useEffect } from 'react';
-import HeaderWithSidebar from '../HeaderWithSidebar';
-import ProductList from './CurrentmonthList';
-import { useDispatch } from 'react-redux';
-import { refreshCustomers } from '../../slices/customerProfile';
-import { refreshLocation } from '../../slices/locationSlice';
+import React, { useEffect } from "react";
+import HeaderWithSidebar from "../HeaderWithSidebar";
+import ProductList from "./CurrentmonthList";
+import { useDispatch } from "react-redux";
+import { refreshCustomers } from "../../slices/customerProfile";
+import { refreshLocation } from "../../slices/locationSlice";
 
 function TopCustomer() {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(refreshCustomers());
-		dispatch(refreshLocation());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(refreshCustomers());
+    dispatch(refreshLocation());
+  }, [dispatch]);
 
-	return (
-		<div>
-			<HeaderWithSidebar />
-			<ProductList />
-		</div>
-	);
+  return (
+    <div>
+      <HeaderWithSidebar />
+      <ProductList />
+    </div>
+  );
 }
 
 export default TopCustomer;
