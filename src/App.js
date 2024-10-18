a9f4b398c5522d2e1a678cd68deb60fcd960c37d
Abhishek Singh <abhisheksingh4115@gmail.com>
2024-10-18 10:18:14 +0100

    formate

--- src/App.js
+++ src/App.js
@@ -1,173 +1,177 @@
 /** @format */
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getMe } from "./service/operations/authApi";
-import ProtectedRoute from "./utils/ProtectedRoute";
-import { Toaster } from "react-hot-toast";
-import LocationStep from "./components/LocationStep";
-import AboutStep from "./components/AboutStep";
-import ServiceStep from "./components/ServiceStep";
-import AuthForm from "./sign-in/AuthForm";
-import Location from "./components/Location/Location";
-import Customers from "./components/Customers/Customers";
-import Products from "./components/Products/Products";
-import Services from "./components/Services/Services";
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	useNavigate,
+} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMe } from './service/operations/authApi';
+import ProtectedRoute from './utils/ProtectedRoute';
+import { Toaster } from 'react-hot-toast';
+import LocationStep from './components/LocationStep';
+import AboutStep from './components/AboutStep';
+import ServiceStep from './components/ServiceStep';
+import AuthForm from './sign-in/AuthForm';
+import Location from './components/Location/Location';
+import Customers from './components/Customers/Customers';
+import Products from './components/Products/Products';
+import Services from './components/Services/Services';
 // import Transactions from './components/Transactions/Transactions';
-import Users from "./components/Users/Users";
-import Allcustomers from "./components/Allcustomers/Allcustomers";
-import Bydata from "./components/Bydata/Bydata";
-import Currentmonth from "./components/Currentmonth/Currentmonth";
-import Topcustomers from "./components/Topcustomers/Topcustomers";
-import Serviceused from "./components/Serviceused/Serviceused";
-import Purchasereport from "./components/Purchasereport/Purchasereport";
-import Productreport from "./components/Productreport/Productreport";
-import Qrcode from "./components/Qrcode/Qrcode";
-import "./App.css";
-import TopHeader from "./components/TopHeader";
-import { getStats } from "./service/operations/statApi";
-import { Dashboard } from "@mui/icons-material";
-import TopCustomer from "./components/Currentmonth/Currentmonth";
+import Users from './components/Users/Users';
+import Allcustomers from './components/Allcustomers/Allcustomers';
+import Bydata from './components/Bydata/Bydata';
+import Currentmonth from './components/Currentmonth/Currentmonth';
+import Topcustomers from './components/Topcustomers/Topcustomers';
+import Serviceused from './components/Serviceused/Serviceused';
+import Purchasereport from './components/Purchasereport/Purchasereport';
+import Productreport from './components/Productreport/Productreport';
+import Qrcode from './components/Qrcode/Qrcode';
+import './App.css';
+import TopHeader from './components/TopHeader';
+import { getStats } from './service/operations/statApi';
+import { Dashboard } from '@mui/icons-material';
+import TopCustomer from './components/Currentmonth/Currentmonth';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const [selectedLocation, setSelectedLocation] = useState(0);
-  const [stats, setStats] = useState({});
-  const {
-    token,
-    loading,
-    user: loginUser,
-  } = useSelector((state) => state.auth);
-  const navigate = useNavigate();
-  // Fetch user info on app load if token is available
-  useEffect(() => {
-    if (token) {
-      dispatch(getMe(navigate));
-    }
-  }, [dispatch, token]);
+	const dispatch = useDispatch();
+	const [selectedLocation, setSelectedLocation] = useState(0);
+	const [stats, setStats] = useState({});
+	const {
+		token,
+		loading,
+		user: loginUser,
+	} = useSelector((state) => state.auth);
+	const navigate = useNavigate();
 
-  const handleLocationChange = (e) => {
-    setSelectedLocation(Number(e.target.value));
-  };
+	// Fetch user info on app load if token is available
+	useEffect(() => {
+		if (token) {
+			dispatch(getMe(navigate));
+		}
+	}, [dispatch, token]);
 
-  return (
-    <div className="app-container">
-      {loginUser && (
-        <TopHeader
-          setSelectedLocation={setSelectedLocation}
-          selectedLocation={selectedLocation}
-          handleLocationChange={handleLocationChange}
-        />
-      )}
-      <Routes>
-        <Route path="/" element={<AuthForm />} />
-        {/* Protected routes */}
-        {/* <Route
+	const handleLocationChange = (e) => {
+		setSelectedLocation(Number(e.target.value));
+	};
+
+	return (
+		<div className='app-container'>
+			{loginUser && (
+				<TopHeader
+					setSelectedLocation={setSelectedLocation}
+					selectedLocation={selectedLocation}
+					handleLocationChange={handleLocationChange}
+				/>
+			)}
+			<Routes>
+				<Route
+					path='/'
+					element={<AuthForm />}
+				/>
+				{/* Protected routes */}
+				{/* <Route
 					path='/locationStep'
 					element={<ProtectedRoute element={<LocationStep />} />}
 				/> */}
-        {/* <Route
+				{/* <Route
 					path='/dashboard'
 					element={<ProtectedRoute element={<Dashboard stats={stats} />} />}
 				/> */}
-        <Route
-          path="/about"
-          element={
-            <ProtectedRoute
-              element={
-                <AboutStep
-                  stats={stats}
-                  setSelectedLocation={setSelectedLocation}
-                  selectedLocation={selectedLocation}
-                  setStats={setStats}
-                />
-              }
-            />
-          }
-        />
-        <Route
-          path="/service"
-          element={
-            <ProtectedRoute
-              element={
-                <ServiceStep
-                  stats={stats}
-                  selectedLocation={selectedLocation}
-                />
-              }
-            />
-          }
-        />
-        {loginUser?.role === "admin" && (
-          <>
-            <Route
-              path="/location"
-              element={<ProtectedRoute element={<Location />} />}
-            />
-            <Route
-              path="/users"
-              element={<ProtectedRoute element={<Users />} />}
-            />
-            <Route
-              path="/services"
-              element={<ProtectedRoute element={<Services />} />}
-            />
-            <Route
-              path="/allcustomers"
-              element={<ProtectedRoute element={<Allcustomers />} />}
-            />
-            <Route
-              path="/bydata"
-              element={<ProtectedRoute element={<Bydata />} />}
-            />
-            <Route
-              path="/topcustomers"
-              element={<ProtectedRoute element={<TopCustomer />} />}
-            />
-            {/* <Route
+				<Route
+					path='/about'
+					element={
+						<ProtectedRoute
+							element={
+								<AboutStep
+									stats={stats}
+									setSelectedLocation={setSelectedLocation}
+									selectedLocation={selectedLocation}
+									setStats={setStats}
+								/>
+							}
+						/>
+					}
+				/>
+				<Route
+					path='/service'
+					element={
+						<ProtectedRoute
+							element={
+								<ServiceStep
+									stats={stats}
+									selectedLocation={selectedLocation}
+								/>
+							}
+						/>
+					}
+				/>
+				{loginUser?.role === 'admin' && (
+					<>
+						<Route
+							path='/location'
+							element={<ProtectedRoute element={<Location />} />}
+						/>
+						<Route
+							path='/users'
+							element={<ProtectedRoute element={<Users />} />}
+						/>
+						<Route
+							path='/services'
+							element={<ProtectedRoute element={<Services />} />}
+						/>
+						<Route
+							path='/allcustomers'
+							element={<ProtectedRoute element={<Allcustomers />} />}
+						/>
+						<Route
+							path='/bydata'
+							element={<ProtectedRoute element={<Bydata />} />}
+						/>
+						<Route
+							path='/topcustomers'
+							element={<ProtectedRoute element={<TopCustomer />} />}
+						/>
+						{/* <Route
 							path='/topcustomers'
 							element={<ProtectedRoute element={<Topcustomers />} />}
 						/> */}
-            <Route
-              path="/serviceused"
-              element={<ProtectedRoute element={<Serviceused />} />}
-            />
-            <Route
-              path="/purchasereport"
-              element={<ProtectedRoute element={<Purchasereport />} />}
-            />
-            <Route
-              path="/productreport"
-              element={<ProtectedRoute element={<Productreport />} />}
-            />
-            {/* <Route
+						<Route
+							path='/serviceused'
+							element={<ProtectedRoute element={<Serviceused />} />}
+						/>
+						<Route
+							path='/purchasereport'
+							element={<ProtectedRoute element={<Purchasereport />} />}
+						/>
+						<Route
+							path='/productreport'
+							element={<ProtectedRoute element={<Productreport />} />}
+						/>
+						{/* <Route
 							path='/qrcode'
 							element={<ProtectedRoute element={<Qrcode />} />}
 						/> */}
-          </>
-        )}
-        <Route
-          path="/products"
-          element={<ProtectedRoute element={<Products />} />}
-        />
+					</>
+				)}
+				<Route
+					path='/products'
+					element={<ProtectedRoute element={<Products />} />}
+				/>
 
-        <Route
-          path="/customers"
-          element={<ProtectedRoute element={<Customers />} />}
-        />
-        {/* <Route
+				<Route
+					path='/customers'
+					element={<ProtectedRoute element={<Customers />} />}
+				/>
+				{/* <Route
 					path='/transactions'
 					element={<ProtectedRoute element={<Transactions />} />}
 				/> */}
-      </Routes>
-    </div>
-  );
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
